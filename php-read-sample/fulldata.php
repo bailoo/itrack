0db84e3a75972d1d3897a02d74851e5063b577b1
Abhishek Singh Bailoo <abhishek.singh.bailoo@gmail.com>
2015-05-12 15:37:30 +0530

    use new function getImeiDateTimes; functions dbQuery* not supported anymore

--- php-read-sample/fulldata.php
+++ php-read-sample/fulldata.php
@@ -5,7 +5,8 @@
 	
 	$o_cassandra = new Cassandra();
 	
-	$s_server_host     = '52.74.33.255';    // Localhost
+	$s_server_host     = '52.74.33.255';
+	//$s_server_host     = '127.0.0.1';    // Localhost
 	$i_server_port     = 9042; 
 	$s_server_username = '';  // We don't have username
 	$s_server_password = '';  // We don't have password
@@ -14,18 +15,11 @@
 	$o_cassandra->connect($s_server_host, $s_server_username, $s_server_password, $s_server_keyspace, $i_server_port);
 	
 	
-	//$imei = '862170018323731';
-	//$imei = '862170018378487';
-	//$imei = '861074025248991';
-	$imei = '862170011627815';
-	$dateminute1 = '2015-01-29-00-00';
-	$dateminute2 = '2015-01-30-23-30';
-	//echo "dateminute1 = $dateminute1\n dateminute2 = $dateminute2\n";
+	$imei = '359231030125239';
+	$datetime1 = '2014-12-31 23:00:00';
+	$datetime2 = '2015-01-01 00:30:30';
 	
-	//make sure the imeih exist in cassandra
-	//$st_results = dbQueryDateHour($o_cassandra,$imei,$date,$HH);
-	
-	$st_results = dbQueryDateTimeSlice($o_cassandra,$imei,$dateminute1,$dateminute2);
+	$st_results = getImeiDateTimes($o_cassandra,$imei,$datetime1,$datetime2);
 
 	//$full_params = array('a','b','c','d','e','f','i','j','k','l','m','n','o','p','q','r','ci','ax','ay','az','mx','my','mz','bx','by','bz');
 	$params = array('a','b','c','d','ax','ay','az','ci');
@@ -34,7 +28,7 @@
 		
 
 	//printHTML($st_results);
-	// echo 'Execution time: '.$i_execution_time."\n";
+	//echo 'Execution time: '.$i_execution_time."\n";
 	
 	$o_cassandra->close();
 
