6ec66e1c442e90cdc3c67e034df0f256f540e599
Shams <Shams@Lenovo-PC>
2015-06-16 19:16:27 +0530

    application files structure

---
+++ ShamsParwez/2015-06-16/vtsRoot/page_details.php
@@ -0,0 +1,16 @@
+
+	     
+<Div style='background-color:white'>
+    <table width='100%' height='100%' cellspacing="0" cellpadding="0" >
+        <tr>
+        <td><?php include('home.php'); ?></td>
+        
+        </tr>
+        
+      
+     
+    </table> 
+    
+</Div> 
+
+             
