5184427ce35d7cdd975b4eaf84a2f1a644193a8f
Shams <Shams@Lenovo-PC>
2015-06-16 20:02:15 +0530

    application files structure

--- ShamsParwez/2015-06-16/vtsRoot/top_info.php
+++
@@ -1,9 +0,0 @@
-<table cellspacing="0" cellpadding="0"  style=" width:100%" border='0'>
-    <tr>
-        <td class="header" style="font-size: x-small;color: brown">Support Ph No: +91-8090025844,+91-9956001418(24X7)&nbsp;&nbsp;&nbsp;Enquiry/Sales: +91-9935551952</td>
-        <!--<td width='10px'><img src='webroot/images/login.gif'></td>-->
-        <td class="login1" style='width:55px;'>iembsys</td>
-        <td class="login" width='25px'><img src='webroot/images/india.jpg' style="height:16px;width:20px"></td>
-    </tr>
-         
-</table>
