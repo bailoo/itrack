5ddc1a309099ec4d25f23f65e974b66bd192edd6
Rizwan <rizwan@iembsys.com>
2015-06-26 11:45:12 +0530

    Problem of pMap in LastData pull fixed

--- JavaProject/crts_mumbai_8002/src/main/java/in/co/itracksolution/SampleLastDataDelete.java
+++ JavaProject/crts_mumbai_8002/src/main/java/in/co/itracksolution/SampleLastDataDelete.java
@@ -22,7 +22,7 @@ public class SampleLastDataDelete {
 		
 			if (inputStream != null) {
 				prop.load(inputStream);
-				conn = new CassandraConn(prop.getProperty("nodes"), prop.getProperty("keyspace"));
+				conn = new CassandraConn(prop.getProperty("nodes"), prop.getProperty("keyspace"), prop.getProperty("username"), prop.getProperty("password"));
 			
 			} else {
 				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
