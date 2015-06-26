d99c929edc675ff602626358ac0b90a33005aa01
Abhishek Singh Bailoo <abhishek.singh.bailoo@gmail.com>
2015-06-26 17:36:10 +0530

    new function getLogByDate to get all logs for a given imei and date

--- phpApi/libCommon.php
+++ phpApi/libCommon.php
@@ -3,8 +3,8 @@
 $s_server_host     = '52.74.33.255';
 #$s_server_host     = '127.0.0.1';    // Localhost
 $i_server_port     = 9042; 
-$s_server_username = '';  // We don't have username
-$s_server_password = '';  // We don't have password
+$s_server_username = 'bailoo';  // We don't have username
+$s_server_password = 'neon04$IC1';  // We don't have password
 $s_server_keyspace = 'gps';  
 
 $TZ='0530';	// Asia/Kolkata
