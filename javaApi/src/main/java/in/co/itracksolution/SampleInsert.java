b39a19b52a1dc8d31b49909633b613fa3bb6bb7d
Abhishek Singh Bailoo <abhishek.singh.bailoo@gmail.com>
2015-07-29 01:47:16 +0530

    testing

--- javaApi/src/main/java/in/co/itracksolution/SampleInsert.java
+++ javaApi/src/main/java/in/co/itracksolution/SampleInsert.java
@@ -90,8 +90,8 @@ public class SampleInsert {
 		SampleInsert st = new SampleInsert();
 		
 		/* Full Data ('a','b','c','d','e','f','g','i','j','k','l','m','n','o','p','q','r','ci','ax','ay','az','mx','my','mz','bx','by','bz'); */
-		String imei = "123456";
-		String dtime = "2015-06-15 20:17:18";
+		String imei = "1234567";
+		String dtime = "2015-07-28 20:17:18";
 		String fullData = "N;v1.45C;1;46.25148;39.86157;0.06;2;5;3;5;6;6;3;5;0;12.88;abcd;1;0;0;1;0;0;1;0;0";
 		st.insertFulldata(imei, dtime, fullData);
 	
