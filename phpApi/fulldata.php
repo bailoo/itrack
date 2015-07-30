f355634ce2a030f4362119aa15aaccd0f1bef1e7
root <root@ip-10-0-0-19.ap-southeast-1.compute.internal>
2015-07-30 19:20:48 +0530

    testing

--- phpApi/fulldata.php
+++ phpApi/fulldata.php
@@ -8,23 +8,57 @@
 	$o_cassandra->connect($s_server_host, $s_server_username, $s_server_password, $s_server_keyspace, $i_server_port);
 	
 	
-	$imei = '865733021562939';
-	$datetime1 = '2015-06-14 23:59:45';
-	$datetime2 = '2015-06-15 00:00:10';
-	//$imei = '359231030125239';
-	//$datetime1 = '2014-12-31 00:00:00';
-	//$datetime2 = '2015-01-01 23:59:59';
-	
+	$imeiArray = array( 
+'862170018314698',
+'862170018383602',
+'862170016975417',
+'862170018382109',
+'861074027067878',
+'862170018313575',
+'862170018368140',
+'865733021569637',
+'865733021562954',
+'862170018315638',
+'862170016970533',
+'862170018364933',
+'862170014330649',
+'862170018322865',
+'862170018367001',
+'862170017811595',
+'862170018366912',
+'861074027068629',
+'861074027067696',
+'862170018366821',
+'862170017810589',
+'862170018316933',
+'865733021569579',
+'862170018368132',
+'862170018323731',
+'862170018320653',
+'862170014373573',
+'862170018323400'
+);
+	$imei = '862170018383602';
+	$datetime1 = '2015-07-28 19:28:00';
+	$datetime2 = '2015-07-28 19:30:00';
 	$deviceTime = TRUE;	// TRUE for query on index dtime, otherwise stime	
-	$orderAsc = FALSE;	// TRUE for ascending, otherwise descending (default) 
-	//$st_results = getImeiDateTimes($o_cassandra, $imei, $datetime1, $datetime2, $deviceTime, $orderAsc);
+	$orderAsc = TRUE;	// TRUE for ascending, otherwise descending (default) 
 
-	$date = '2015-06-14';
-	$st_results = getLogByDate($o_cassandra, $imei, $date, $deviceTime, $orderAsc);
+	//$len = count($imeiArray);
+	//for ($i=0; $i < $len; $i++)
+	//{
+	//	$imei = $imeiArray[$i];
+		echo ("imei = $imei\n");
+		$st_results = getImeiDateTimes($o_cassandra, $imei, $datetime1, $datetime2, $deviceTime, $orderAsc);
+		//$full_params = array('a','b','c','d','e','f','i','j','k','l','m','n','o','p','q','r','ci','ax','ay','az','mx','my','mz','bx','by','bz');
+		print_r($st_results);
+	//}
+	
 
-
+	//$date = '2015-06-14';
+	//$st_results = getLogByDate($o_cassandra, $imei, $date, $deviceTime, $orderAsc);
 	//$full_params = array('a','b','c','d','e','f','i','j','k','l','m','n','o','p','q','r','ci','ax','ay','az','mx','my','mz','bx','by','bz');
-	print_r($st_results);
+	//print_r($st_results);
 		
 
 	//printHTML($st_results);
--- phpApi/lastdata.php
+++ phpApi/lastdata.php
@@ -8,17 +8,15 @@
 	
 	$o_cassandra->connect($s_server_host, $s_server_username, $s_server_password, $s_server_keyspace, $i_server_port);
 	
-	$imei = '865733021570015';
+	$imei = '862170018383602';
 	$st_results = getLastSeen($o_cassandra,$imei);
-	
 	//$last_params = array('a','b','c','d','e','f','g','i','j','k','l','m','n','o','p','q','r','s','t','u','ci','ax','ay','az','mx','my','mz','bx','by','bz');
 	print_r($st_results);
 		
-	$imei = '865733021570015';
-	$st_results = getLastSeenDateTime($o_cassandra,$imei,"2015-06-18 20:55:20");
-		
+	//$imei = '862170018383602';
+	//$st_results = getLastSeenDateTime($o_cassandra,$imei,"2015-06-18 20:55:20");
 	//$full_params = array('a','b','c','d','e','f','i','j','k','l','m','n','o','p','q','r','ci','ax','ay','az','mx','my','mz','bx','by','bz');
-	print_r($st_results);
+	//print_r($st_results);
 		
 	$o_cassandra->close();
 	
