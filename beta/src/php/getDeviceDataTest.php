94f8716d082a823718ef431442e9b86d966c2ce7
Shams <Shams@Lenovo-PC>
2015-08-07 12:32:37 +0530

    change in android application

--- beta/src/php/getDeviceDataTest.php
+++ beta/src/php/getDeviceDataTest.php
@@ -8,12 +8,10 @@ if($isReport) {
    include_once("../../../../phpApi/Cassandra/Cassandra.php");     //##### INCLUDE CASSANDRA API*/
    include_once("../../../../phpApi/libLog.php");     //##### INCLUDE CASSANDRA API*/    
 } else {  
-    $pathInPieces = explode(DIRECTORY_SEPARATOR ,dirname(__FILE__));
-print_r($pathInPieces);
+   $pathInPieces = explode(DIRECTORY_SEPARATOR ,dirname(__FILE__));
+//print_r($pathInPieces);
+//$pathToRoot=$pathInPieces[0]."/".$pathInPieces[1]."/".$pathInPieces[2]."/".$pathInPieces[3];
 $pathToRoot=$pathInPieces[0]."/".$pathInPieces[1]."/".$pathInPieces[2];
-//echo "pathToRoot11=".$pathToRoot."<br>";
-   // echo "in else";
-   // $pathToRoot=$pathInPieces[0]."/".$pathInPieces[1]."/".$pathInPieces[2]."/".$pathInPieces[3];
     include_once($pathToRoot."/phpApi/Cassandra/Cassandra.php");     //##### INCLUDE CASSANDRA API
   
     include_once($pathToRoot."/phpApi/libLogNew.php");     //##### INCLUDE CASSANDRA API*/    //##### INCLUDE CASSANDRA API*/
