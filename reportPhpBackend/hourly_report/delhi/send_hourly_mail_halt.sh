edfe76174d8d295777bad27f8eb4cac018feb4fd
Rizwan <rizwan@iembsys.com>
2015-09-02 12:35:14 +0530

    delhi

---
+++ reportPhpBackend/hourly_report/delhi/send_hourly_mail_halt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /mnt/itrack/reportPhpBackend/hourly_report/delhi
+php mail_hourly_halt_report.php
+
+
+
---
+++ reportPhpBackend/hourly_report/delhi/test_mail.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /mnt/itrack/ReportPhpBackend/hourly_report/delhi
+php test_mail.php
+
+
+
