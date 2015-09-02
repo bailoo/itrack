ff1919008c2c92831e77688830107868d6e6b6ff
Rizwan <rizwan@iembsys.com>
2015-09-02 11:53:24 +0530

    reportPhpBackend

--- reportPhpBackend/hourly_report/tanker/update_last_processed_time.php
+++ reportPhpBackend/hourly_report/tanker/update_last_processed_time.php
@@ -2,20 +2,34 @@
 
 function updateAll_last_processed_time($type, $last_time,$route_type) {
     //echo "\nLAST PROCESSED TIME";
+    global $LOG;
+    $title = "tanker";
     global $DbConnection;
     global $account_id;
     global $vehicle_imei_rdb;
+    global $DEBUG_OFFLINE;
+    global $DEBUG_ONLINE;
+    global $difftime;
+    $debug_msg = "";
+        
     echo "\nSize=".sizeof($vehicle_imei_rdb);
+    $msg = "\nSizeImei=".sizeof($vehicle_imei_rdb);
+    
+    if($LOG) {$debug_msg.=$msg."\n";}
+    
     foreach($vehicle_imei_rdb as $imei) {
         $query = "SELECT imei FROM last_processed_time USE INDEX(last_processed_time_key) WHERE account_id='$account_id' AND imei='$imei' AND type='$type' AND routetype='$route_type'" ;
         echo "\nQA1=".$query;
         $result = mysql_query($query,$DbConnection);
         $numrows = mysql_num_rows($result);
-        
+        $msg = "\nIMEI=".$imei."\n SELECT QUERY::=>".$query." \nRESULT RESULT::=>".$result." \nNUMROWS::=>".$numrows;     
+        if($LOG) {$debug_msg.=$msg."\n";}
         if($numrows ==0) {
             $query_insert = "INSERT INTO last_processed_time(imei,last_time,account_id,type,routetype) values('$imei','$last_time','$account_id','$type','$route_type')";
             $result_insert = mysql_query($query_insert,$DbConnection); 
             echo "\nQA2=".$query_insert;
+            $msg = "\nIMEI=".$imei."\nSELECT INSERT::=>".$query_insert." \nRESULT INSERT::=>".$result_insert;
+            if($LOG) {$debug_msg.=$msg."\n";}
         } /*else {
             $query_update = "UPDATE last_processed_time SET last_time='$last_time' WHERE account_id='$account_id' AND imei='$imei' AND type='$type' AND routetype='$route_type'";
             $result_update = mysql_query($query_update,$DbConnection); 
@@ -23,8 +37,15 @@ function updateAll_last_processed_time($type, $last_time,$route_type) {
         }*/
         
         $query_update = "UPDATE last_processed_time SET last_time='$last_time' WHERE account_id='$account_id' AND type='$type' AND routetype='$route_type'";
-        $result_update = mysql_query($query_update,$DbConnection); 
+        $result_update = mysql_query($query_update,$DbConnection);
+        
+        $msg = "\nIMEI=".$imei."\nQUERY UPDATE::=>".$query_update." \nRESULT UPDATE::=>".$result_update;
+        if($LOG) {$debug_msg.=$msg."\n";}
+        
         echo "\nQA3=".$query_update;       
+    }
+    if(!$DEBUG_OFFLINE && !$DEBUG_ONLINE && $LOG) {
+        write_log($title,$difftime);
     }
 }
 
