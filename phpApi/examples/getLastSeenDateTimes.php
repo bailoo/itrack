3149e2f61c4642593fe820747a78c6f64b159560
Abhishek Singh Bailoo <abhishek.singh.bailoo@gmail.com>
2015-09-04 09:44:44 +0530

    function getLastSeenDateTimes returns empty object if latitude or longitude are missing

--- phpApi/examples/getLastSeenDateTimes.php
+++ phpApi/examples/getLastSeenDateTimes.php
@@ -8,9 +8,9 @@
 	$o_cassandra->connect($s_server_host, $s_server_username, $s_server_password, $s_server_keyspace, $i_server_port);
 	
 	
-	$imei = '862170017136738';
-	$datetime1 = '2015-08-21 10:00:00';
-	$datetime2 = '2015-08-21 19:00:00';
+	$imei = '865733024479636';
+	$datetime1 = '2015-08-31 00:00:00';
+	$datetime2 = '2015-08-31 23:59:59';
 	$st_results = getLastSeenDateTimes($o_cassandra, $imei, $datetime1, $datetime2);
 	$o_cassandra->close();
 	//$full_params = array('a','b','c','d','e','f','i','j','k','l','m','n','o','p','q','r','ci','ax','ay','az','mx','my','mz','bx','by','bz');
