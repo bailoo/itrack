5288c9cc2a2d4178e0ebe826e5cf32b1910278c0
Rizwan <rizwan@iembsys.com>
2015-09-14 12:11:45 +0530

    itrack

--- reportPhpBackend/test_hourly_report/test_delhi/send_hourly_mail_halt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /mnt/itrack/ReportPhpBackend/hourly_report/delhi
-php mail_hourly_halt_report.php
-
-
-
--- reportPhpBackend/test_hourly_report/wockhardt/send_hourly_mail_halt.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/php
-cd /var/www/html/vts/beta/src/php/hourly_report/wockhardt
-php mail_hourly_wockardt_report.php
-
-
