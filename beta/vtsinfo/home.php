b9536d6dca75c7af3bff2a3c95c946164f648658
Shams <Shams@Lenovo-PC>
2015-09-29 18:37:48 +0530

    change in vts info

--- beta/vtsinfo/home.php
+++ beta/vtsinfo/home.php
@@ -82,17 +82,17 @@ echo'
     <br>    
     <table align="center" style="font-size:14px;">      
       <tr>
-        <!--<td id="cell1" align="center" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(1);"><strong><font color="#610B0B">Account/Vehicle/IMEI Detail&nbsp;</font></strong></td>
+        <td id="cell1" align="center" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(1);"><strong><font color="#610B0B">Account/Vehicle/IMEI Detail&nbsp;</font></strong></td>
         <td id="cell8" align="center" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(8);"><strong><font color="#610B0B">Suspend Account&nbsp;</font></strong></td>
-		<td id="cell9" align="center" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(9);"><strong><font color="#610B0B">Continue Account&nbsp;</font></strong></td>-->
+		<td id="cell9" align="center" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(9);"><strong><font color="#610B0B">Continue Account&nbsp;</font></strong></td>
 		<!--<td id="cell2" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(2);"><strong><font color="#610B0B">Get group&nbsp;</font><font color=green>(BETA)</font></strong></td>        
-       <!--<td id="cell3" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(3);"><strong><font color="#610B0B">Vehicle detail&nbsp;</font><font color="blue">(OLD)</font></strong></td>-->                
-        <td id="cell4" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(4);"><strong><font color="#610B0B"><strong>XML Check&nbsp;</strong></font></td>-->
+       <td id="cell3" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(3);"><strong><font color="#610B0B">Vehicle detail&nbsp;</font><font color="blue">(OLD)</font></strong></td>-->                
+        <td id="cell4" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(4);"><strong><font color="#610B0B"><strong>XML Check&nbsp;</strong></font></td>
         <!--<td id="cell5" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(5);"><strong><font color="#610B0B"><strong>Avg IO&nbsp;</strong></font></td>-->
         <td id="cell6" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(6);"><strong><font color="#610B0B"><strong>Data Download&nbsp;</strong></font></td>
         <!--<td id="cell7" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(7);"><strong><font color="#610B0B"><strong>Debug account&nbsp;</strong></font></td>-->
-		<!--<td id="cell10" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(10);"><strong><font color="#610B0B"><strong>Device History&nbsp;</strong></font></td>
-		<td id="cell11" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(11);"><strong><font color="#610B0B"><strong>SMS Detail&nbsp;</strong></font></td>-->
+		<td id="cell10" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(10);"><strong><font color="#610B0B"><strong>Device History&nbsp;</strong></font></td>
+		<td id="cell11" style="cursor:pointer" onmouseover="changeColor(\'F7BE81\', this.id);" onmouseout="changeColor(\'FFFFFF\', this.id);" onclick="javascript:redirect(11);"><strong><font color="#610B0B"><strong>SMS Detail&nbsp;</strong></font></td>
       </tr>     
     </table>  
   </body>
