65483831f189e51cd5f9ff686a113487e504a246
taseen <taseen@Innovative-Lenovo-PC>
2015-10-19 10:45:43 +0530

    love_location_prev_invoiceN

--- beta/src/php/live_location_prev_invoiceN.php
+++ beta/src/php/live_location_prev_invoiceN.php
@@ -268,7 +268,7 @@ $for_action1=$_POST['title'];
 			{		
 				//alert("distance="+distance+"address2="+address2);
 				//alert("latLng="+latLng);
-				 var strURL="src/php/get_location_tmp_file.php?point_test="+latLng;
+				 var strURL="get_location_tmp_file.php?point_test="+latLng;
 				//alert("strurl:"+strURL);
 				var req = getXMLHTTP();
 				req.open("GET", strURL, false); //third parameter is set to false here
