423be888c36bbd817724adfc922d930902ea236f
Shams <Shams@192.168.1.103>
2015-11-07 11:49:46 +0530

    change logic in last position

--- beta/src/php/getXmlData.php
+++ beta/src/php/getXmlData.php
@@ -916,34 +916,31 @@ function readFileXmlNew($vSerial, $dateToData,  $requiredData, $sortBy, $paramet
 function getLastPositionXMl($vSerial,$startDate,$endDate,$xmlFromDate,$xmlToDate,$sortBy,$type,$parameterizeData,&$dataObject)
 {
    global $o_cassandra; 
-   if ($sortBy == "h")
-   {
-        $deviceTime=TRUE;
-   }
-   else if($sortBy == "g")
-   {
-        $deviceTime=FALSE;
-   }
- 
-    $lastRecordArr=$st_results = getLastSeen($o_cassandra,$vSerial);
-    //var_dump($lastRecordArr);
-    foreach($lastRecordArr as $itemLR) 
+    if ($sortBy == "h")
     {
-        $last_halt_time = $itemLR->u;
-        $day_max_spd=$itemLR->s;
+        $deviceTime=TRUE;
+    }
+    else if($sortBy == "g")
+    {
+        $deviceTime=FALSE;
     }
     
-    //echo "last_halt_time=".$last_halt_time."day_max_spd=".$day_max_spd."<br>";
-    
-    $deviceTime = TRUE;	// TRUE for query on index dtime, otherwise stime	
-    $orderAsc = TRUE;	// TRUE for ascending, otherwise descending (default)   
-    //$st_results = getImeiDateTimes($o_cassandra, $vSerial, $startDate, $endDate, $deviceTime, $orderAsc);
-    $st_results = getLastSeenDateTimes($o_cassandra, $vSerial, $startDate, $endDate);
-    //print_r($st_results);
-    //echo "countAr=".count($st_results)."<br>";
-    if(count($st_results)>0)
+    $dataFoundFlag=0;
+    $lastDataAmongData=0;
+    if(strtotime(date('Y-m-d H:i:s'))-strtotime($endDate)<1800)
     {
-        //echo "lat".$st_results->d."<br>";
+        $dataFoundFlag=1;
+        $st_results = getLastSeen($o_cassandra,$vSerial);
+    }
+    else
+    {
+        $dataFoundFlag=1;
+        $lastDataAmongData=1;
+        $st_results = getLastSeenDateTimes($o_cassandra, $vSerial, $startDate, $endDate);
+    }
+    
+    if($dataFoundFlag==1)
+    {
         $msg_type = $st_results->a;                 
         $ver = $st_results->b;              
         $fix = $st_results->c;
@@ -962,7 +959,17 @@ function getLastPositionXMl($vSerial,$startDate,$endDate,$xmlFromDate,$xmlToDate
         $io7 = $st_results->o;
         $io8 = $st_results->p;
         $sig_str = $st_results->q;
-        $sup_v = $st_results->r;
+        $sup_v = $st_results->r; 
+        if($lastDataAmongData==0)
+        {
+        $last_halt_time = $itemLR->u;
+        $day_max_spd=$itemLR->s;
+        }
+        else
+        {
+            $last_halt_time = '-';
+            $day_max_spd='-'; 
+        }
         $DataValid = 0;
         if ($parameterizeData->latitude != null && $parameterizeData->longitude != null) 
         {
@@ -1040,7 +1047,7 @@ function getLastPositionXMl($vSerial,$startDate,$endDate,$xmlFromDate,$xmlToDate
             }            
             if ($parameterizeData->lastHaltTime != null) 
             {
-                $last_halt_time_1 = $last_halt_time;
+                    $last_halt_time_1 = $last_halt_time;
             }
 
             if ($parameterizeData->latitude != null && $parameterizeData->longitude != null) 
@@ -1057,7 +1064,7 @@ function getLastPositionXMl($vSerial,$startDate,$endDate,$xmlFromDate,$xmlToDate
 
             if ($parameterizeData->sigStr != null) 
             {
-                $sig_str_1 = $sig_str;
+                    $sig_str_1 = $sig_str;
             }
             if ($parameterizeData->supVoltage != null) 
             {
@@ -1065,13 +1072,14 @@ function getLastPositionXMl($vSerial,$startDate,$endDate,$xmlFromDate,$xmlToDate
             }
         }
         /*foreach($st_results as $item) 
-        {          
+        {
+          
             $msg_type = $item->a;                 
             $ver = $item->b;              
             $fix = $item->c;
             $lat = $item->d;
             $lng = $item->e;
-            //echo "lat".$lat."<br>";
+            echo "lat".$lat."<br>";
             $speed = $item->f;
             $datetime_server = str_replace('@',' ',$item->g);
             $datetime_device = str_replace('@',' ',$item->h);              
