a96ee1bcdfd8ce063d2908d9b3495449cda8f7c3
Shams <Shams@Lenovo-PC>
2015-12-03 13:22:43 +0530

    change in klpServer side report web service

--- beta/src/php/select_landmark_marker.php
+++ beta/src/php/select_landmark_marker.php
@@ -18,49 +18,45 @@
   
   $placename1 ="";
   $i=0;
-  while($row=mysql_fetch_object($result))
-  {
-  	$landmark=$row->landmark_name;
-  	$coord = $row->landmark_coord;
-  	
-  	$coord1 = explode(',',$coord);
-  	$lat2= $coord1[0];
-  	$lng2= $coord1[1];
-
-		//echo "lat1=".$lat1."long1=".$lon1."lat2=".$lat2."long2=".$lon2;
-		calculate_distance($lat1,$lat2,$lng1,$lng2,$distance);
-		$distance1=round($distance,2);
-		
-		if($i==0)
-		{
-		  $lowest_dist = $distance1;
-		  $placename1 = $landmark;
-    }
-    else
+    while($row=mysql_fetch_object($result))
     {
-      if($distance1 < $lowest_dist)
-      {
-        $lowest_dist = $distance1;
-        $placename1 = $landmark;
-      } 
+        $landmark=$row->landmark_name;
+        $coord = $row->landmark_coord;
+
+        $coord1 = explode(',',$coord);
+        $lat2= isset($coord1[0])?$coord1[0]:'';
+        $lng2= isset($coord1[1])?$coord1[1]:'';
+        if(strlen($lat2>4) && strlen($lng2>4))
+        {
+            calculate_distance($lat1,$lat2,$lng1,$lng2,$distance);
+            $distance1=round($distance,2);
+            if($i==0)
+            {
+                $lowest_dist = $distance1;
+                $placename1 = $landmark;
+            }
+            else
+            {
+                if($distance1 < $lowest_dist)
+                {
+                    $lowest_dist = $distance1;
+                    $placename1 = $landmark;
+                } 
+            }
+            $i++;
+        }
     }
-	
-    $i++;	
-		//echo "dist=".$distance;
-		//echo "lat1=".$lat1."long1=".$lon1."distance=".$distance."lat2=".$lat2."long2=".$lon2;;
-	}
-	$query1 = "SELECT distance_variable FROM account_detail WHERE account_id='$account_id'";
-	$result1 = mysql_query($query1,$DbConnection); 	
-	$Row1=mysql_fetch_object($result1);
-	$distance_variable1=$Row1->distance_variable;
-	$placename2="";
-	if(($lowest_dist <=$distance_variable1) && ($placename1!=""))
-	{
-		$placename2 = $lowest_dist." km from ".$placename1;	 
-		//echo "<br>place=".$placename2;
-	}
-    
-	echo $placename2;	
-	//$distance1=round($distance,4);*/
+    $query1 = "SELECT distance_variable FROM account_detail WHERE account_id='$account_id'";
+    $result1 = mysql_query($query1,$DbConnection); 	
+    $Row1=mysql_fetch_object($result1);
+    $distance_variable1=$Row1->distance_variable;
+    $placename2="";
+    if(($lowest_dist <=$distance_variable1) && ($placename1!=""))
+    {
+        $placename2 = $lowest_dist." km from ".$placename1;	 
+        //echo "<br>place=".$placename2;
+    }
+    echo $placename2;	
+    //$distance1=round($distance,4);*/
 	
 ?>
