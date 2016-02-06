7969f8fdb5739534b2fc31902fee4cd913bacc88
taseen <taseen@192.168.1.119>
2016-02-06 15:46:41 +0530

    alert polyline route deviation

--- beta/src/php/route_deviation_alert_db/setup/database_conn.php
+++ beta/src/php/route_deviation_alert_db/setup/database_conn.php
@@ -4,7 +4,7 @@
         //$USER = "root";
 	$PASSWD = 'neon04$VTS';
 	$HOST = "itrackdb.c4pqfsdaiccz.us-east-1.rds.amazonaws.com";
-        $HOST='localhost';
+        //$HOST='localhost';
 	$DbConnection = mysql_connect($HOST,$USER,$PASSWD) or die("could not connect to DB test");
 	mysql_select_db ($DBASE, $DbConnection) or die("could not find DB");
 	$common_date=date("Y-m-d H:i:s");
