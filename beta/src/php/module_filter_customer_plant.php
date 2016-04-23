d9e6dfd8ab16ec41044279fae8f59cfcbd63a4de
Rizwan <rizwan@iembsys.com>
2016-04-23 17:31:25 +0530

    module_filter_customer_plant

--- beta/src/php/module_filter_customer_plant.php
+++ beta/src/php/module_filter_customer_plant.php
@@ -1,116 +1,70 @@
 </td>
 <td class="mystyle">
-<form method="POST" action="customer_plant_home.php">
-<input type="hidden" name="filter_mode" value="1">
-<div style="background-color:lightgrey" align="center"><h3>Filter Customer Plant</h3></center></div>
-<div align="left">
-<?php
-if($filter_mode ==1 && $customer=="") {
-echo '<input type="checkbox" id="customer" name="customer" onclick="javascript:show_hide_option(\'customer\');">&nbsp;<strong>Customer</strong>';
-} else {
-echo '<div align="left"><input type="checkbox" id="customer" name="customer" checked onclick="javascript:show_hide_option(\'customer\');">&nbsp;<strong>Customer</strong>';
-}
+    <form method="POST" action="customer_plant_home.php" name="form1">
+        <input type="hidden" name="filter_flag"  value="1">
+        <div style="background-color:lightgrey" align="center"><h3>Filter Customer Plant</h3></center></div>
 
-if($filter_mode ==1 && $plant=="") {
-echo '&nbsp;&nbsp;&nbsp;<input type="checkbox" id="plant" name="plant" onclick="javascript:show_hide_option(\'plant\');">&nbsp;<strong>Plant</strong>';
-} else {
-echo '&nbsp;&nbsp;&nbsp;<input type="checkbox" id="plant" name="plant" checked onclick="javascript:show_hide_option(\'plant\');">&nbsp;<strong>Plant</strong>';
-}
-?>
-</div>
-<br>
-<table align="left" width="100%">
-		<tr><td colspan="2"></td></tr>
+        <table align="left" width="100%">
 
-		<tr><td></td></tr>
-		<?php
-		if( ($filter_mode == "") || ($filter_mode ==1 && $customer=="on") ) {
-			echo '
-			<tr id="customer_row"><td><strong>Customer option</strong></td><td>
-			
-				<select id="customer_option" name="customer_option">
-				<option value="All">Select filter type</option>';
+            <tr><td>							
+                    <table align="left">
+                        <tr><td>&nbsp;<strong>Select Station Groups</strong></td></tr>
+                        <?php
+                        if ($All == 'on') {
+                            echo '<tr><td><input type="checkbox" name="All" checked Onchange="javascript:selectUnselectAll();"> &nbsp;<font color="#303036"><strong>All</strong></font></td></tr>';
+                        } else {
+                            echo '<tr><td><input type="checkbox" name="All" Onchange="javascript:selectUnselectAll();"> &nbsp;<font color="#303036"><strong>All</strong></font></td></tr>';
+                        }
+                        if ($MS == 'on') {
+                            echo '<tr><td><input type="checkbox" name="MS" checked> &nbsp;<div class="tooltip"><font color="#269900"><strong>MS</strong></font><span class="tooltiptext">(1->1999,<br>17001->19999)</span></div></td></tr>';
+                        } else {
+                            echo '<tr><td><input type="checkbox" name="MS"> &nbsp;<div class="tooltip"><font color="#269900"><strong>MS</strong></font><span class="tooltiptext">(1->1999,<br>17001->19999)</span></div></td></tr>';
+                        }
 
-					if($filter_mode ==1 && $customer_option!="") {
-						echo '<option value="'.$customer_option.'" selected>'.$customer_option.'</option>';
-					}
-				echo '
-				<option value="All">All</option>
-				<option value="FS">FS</option>
-				<option value="MS">MS</option>
-				<option value="dISTT">dISTT</option>
-				<option value="Dist">Dist</option>
-				<option value="DMS">DMS</option>
-				<option value="UPSC">UPSC</option>
-				<option value="Institution">Institution</option>
-				<option value="INS">INS</option>
-					
-				</select></td>
-			</tr>';
-		} else {
-			echo '
-			<tr id="customer_row" style="display:none;"><td><strong>Customer option</strong></td><td>			
-				<select id="customer_option" name="customer_option">
-				<option value="All">Select filter type</option>
-				<option value="All">All</option>
-				<option value="FS">FS</option>
-				<option value="MS">MS</option>
-				<option value="dISTT">dISTT</option>
-				<option value="Dist">Dist</option>
-				<option value="DMS">DMS</option>
-				<option value="UPSC">UPSC</option>
-				<option value="Institution">Institution</option>
-				<option value="INS">INS</option>					
-				</select></td>
-			</tr>';		
-		}
-		
-		if( ($filter_mode == "") || ($filter_mode ==1 && $plant=="on") ) {	
-		
-			echo '<tr id="plant_row"><td><strong>Plant option</strong></td><td>
-				<select id="plant_option" name="plant_option">';
+                        if ($KISOK == 'on') {
+                            echo '<tr><td><input type="checkbox" name="KISOK" checked> &nbsp;<div class="tooltip"><font color="#990099"><strong>KISOK</strong></font><span class="tooltiptext">(14001->15999)</span></div></td></tr>';
+                        } else {
+                            echo '<tr><td><input type="checkbox" name="KISOK"> &nbsp;<div class="tooltip"><font color="#990099"><strong>KISOK</strong></font><span class="tooltiptext">(14001->15999)</span></div></td></tr>';
+                        }
 
-				if($filter_mode ==1 && $plant_option!="") {
-					echo '<option value="'.$plant_option.'" selected>'.$plant_option.'</option>';
-				}		
-				echo '<option value="All">Select filter type</option>
-				<option value="All">All</option>			
-				
-				</select>
-				</td>
-			</tr>';
-		} else {
-			echo '<tr id="plant_row" style="display:none;"><td><strong>Plant option</strong></td><td>
-				<select id="plant_option" name="plant_option">
-				<option value="All">Select filter type</option>
-				<option value="All">All</option>				
-				</select>
-				</td>
-			</tr>';
-		}
-		?>
-		
-</table>
-<br><br><br><br>
-<div align="left"><input type="Submit" value="Show List"></div>
+                        if ($INSTITUTION == 'on') {
+                            echo '<tr><td><input type="checkbox" name="INSTITUTION" checked> &nbsp;<div class="tooltip"><font color="#000099"><strong>INSTITUTION</strong></font><span class="tooltiptext">(1000001->1999999,<br>5000001->5999999)</span></div></td></tr>';
+                        } else {
+                            echo '<tr><td><input type="checkbox" name="INSTITUTION"> &nbsp;<div class="tooltip"><font color="#000099"><strong>INSTITUTION</strong></font><span class="tooltiptext">(1000001->1999999,<br>5000001->5999999)</span></div></td></tr>';
+                        }
 
-<div align="left" style="background-color:lightgrey"><h4>Customer Plant List</h4></div>	
-	
-	
-<div id="customer_plant_id" overflow="auto" style="border: 1px solid red; height:420px; width:250px; position:relative; overflow:auto">
-<?php
-/*for($i=0;$i<sizeof($customer_no);$i++) {
-	
-	echo '<a href="javascript:highlight_customer('.$customer_no[$i].','.$lat[$i].','.$lng[$i].');"><strong>'.$customer_no[$i].'&nbsp;(<font color=green>'.$customer_name[$i].'</font>)</strong></a><br><br>';	
-}*/
+                        if ($FS == 'on') {
+                            echo '<tr><td><input type="checkbox" name="FS" checked> &nbsp;<div class="tooltip"><font color="#990000"><strong>FS</strong></font><span class="tooltiptext">(12001->13999,<br>16001->16999)</span></div></td></tr>';
+                        } else {
+                            echo '<tr><td><input type="checkbox" name="FS"> &nbsp;<div class="tooltip"><font color="#990000"><strong>FS</strong></font><span class="tooltiptext">(12001->13999,<br>16001->16999)</span></div></td></tr>';
+                        }
 
-?>
-</div>
+                        if ($DISTRIBUTOR == 'on') {
+                            echo '<tr><td><input type="checkbox" name="DISTRIBUTOR" checked> &nbsp;<div class="tooltip"><font color="#007399"><strong>DISTRIBUTOR</strong></font><span class="tooltiptext">(70001->77999)</span></div></td></tr>';
+                        } else {
+                            echo '<tr><td><input type="checkbox" name="DISTRIBUTOR"> &nbsp;<div class="tooltip"><font color="#007399"><strong>DISTRIBUTOR</strong></font><span class="tooltiptext">(70001->77999)</span></div></td></tr>';
+                        }
 
-</form>
+                        if ($PLANTS == 'on') {
+                            echo '<tr><td><input type="checkbox" name="PLANTS" checked> &nbsp;<div class="tooltip"><font color="#ff0000"><strong>PLANTS</strong></font><span class="tooltiptext">(ALL)</span></div></td></tr>';
+                        } else {
+                            echo '<tr><td><input type="checkbox" name="PLANTS"> &nbsp;<div class="tooltip"><font color="#ff0000"><strong>PLANTS</strong></font><span class="tooltiptext">(ALL)</span></div></td></tr>';
+                        }
+                        ?>
+                        
+                    </table>				
+                </td>
+            </tr>	
+        </table>
 
-<script type="text/javascript">
-	document.getElementById('customer_plant_id').innerHTML = document.getElementById('customer_list').value;
-</script>
+        <div align="left"><input type="Submit" value="Show List >>"></div>
+
+        <div align="left" style="background-color:lightgrey"><h4>Customer Plant List</h4></div>	
+
+        <div id="customer_plant_id" overflow="auto" style="border: 1px solid red; height:340px; width:250px; position:relative; overflow:auto">
+            
+        </div>
+
+    </form>
 
 </td>
