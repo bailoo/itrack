d96e18a2d92d562d117ea4b4d6b4fa12cfcb5f3d
lenovo <lenovo@lenovo-PC>
2016-05-20 01:14:48 +0530

    1

--- beta/src/php/coreDb.php
+++ beta/src/php/coreDb.php
@@ -134,9 +134,15 @@ function stationRecord($accId,$DbConnection)
     return $dataArr;
 }
 
-function getAccountRoutes($accId,$DbConnection)
+function getAccountRoutes($accId,$route_names,$DbConnection)
 {
-    $Query="SELECT polyline_id,polyline_name,polyline_coord FROM polyline WHERE user_account_id=$accId AND status=1";
+    $Query="SELECT DISTINCT polyline_id,polyline_name,polyline_coord FROM polyline WHERE ".
+            "polyline_name IN($route_names) AND ".
+            "user_account_id=$accId AND status=1";
+    /*$Query="SELECT DISTINCT polyline_id,polyline_name,polyline_coord FROM polyline WHERE ".
+            "user_account_id=$accId AND status=1";*/
+    //echo $Query;
+    //exit();
     $Result=mysql_query($Query,$DbConnection);
     $numRows=  mysql_num_rows($Result);
     if($numRows>0)
