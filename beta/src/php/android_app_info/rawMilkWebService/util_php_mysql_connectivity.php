6a17a09da5b0b1d17045beca902c611f2a167934
Shams <Shams@Lenovo-PC>
2016-05-24 17:10:38 +0530

    database credentials changed

--- beta/src/php/android_app_info/rawMilkWebService/util_php_mysql_connectivity.php
+++ beta/src/php/android_app_info/rawMilkWebService/util_php_mysql_connectivity.php
@@ -1,9 +1,11 @@
 <?php
+    $DBASE = "iespl_vts_beta";
 	//include_once("PhpMysqlConnectivity.php");
-	$HOST = "itrackdb.c4pqfsdaiccz.us-east-1.rds.amazonaws.com";
-	$DBASE = "iespl_vts_beta";
-	$USER = "bailoo";
-	$PASSWD = 'neon04$VTS';
-	$DbConnection = mysql_connect($HOST,$USER,$PASSWD) or die("could not connect to DB test");
-	mysql_select_db ($DBASE, $DbConnection) or die("could not find DB");
+    $USER = "nimbumirchidb";
+    //$USER = "root";
+    $PASSWD = 'nm#db0516';
+    //$PASSWD = 'mysql';
+    $HOST = "nimbumirchi.c4pqfsdaiccz.us-east-1.rds.amazonaws.com";
+    $DbConnection = mysql_connect($HOST,$USER,$PASSWD) or die("could not connect to DB test");
+    mysql_select_db ($DBASE, $DbConnection) or die("could not find DB");
 ?>
