3de6b634b279d25caec02b18c901c092e3b45779
Shams <Shams@Lenovo-PC>
2016-05-24 17:09:35 +0530

    database credentials changed

--- beta/src/php/android_app_info/util_android_php_mysql_connectivity.php
+++ beta/src/php/android_app_info/util_android_php_mysql_connectivity.php
@@ -1,10 +1,10 @@
 <?php
     $DBASE = "iespl_vts_beta";
-    $USER = "bailoo";
+    $USER = "nimbumirchidb";
     //$USER = "root";
-    $PASSWD = 'neon04$VTS';
+    $PASSWD = 'nm#db0516';
     //$PASSWD = 'mysql';
-    $HOST = "itrackdb.c4pqfsdaiccz.us-east-1.rds.amazonaws.com";
+    $HOST = "nimbumirchi.c4pqfsdaiccz.us-east-1.rds.amazonaws.com";
     //$HOST = "localhost";
     $DbConnection = mysql_connect($HOST,$USER,$PASSWD) or die("Connection to server is down. Please try after few minutes.");
     mysql_select_db ($DBASE, $DbConnection) or die("could not find DB");
