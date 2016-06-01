b7df501ba73e2b9634f7167b5e149329717bd77c
taseen <taseen@Innovative-Lenovo-PC>
2016-06-01 12:57:21 +0530

    look and feel all frame outer php

--- beta/src/php/module_frame_header.php
+++ beta/src/php/module_frame_header.php
@@ -179,28 +179,28 @@ echo'
                         {
                           if($interface == "setting.php")
                           {
-                                  echo '<li class="active"><a href="setting.htm" style="padding: 5px 5px;"><span class="fa fa-cog">&nbsp;Setting<span></a></li>';
+                                  echo '<li class="active"><a href="setting.htm" style="padding: 5px 5px;"><span class="fa fa-cog">&nbsp;Setting</span></a></li>';
                           } 
                           else
                           { 
-                                  echo '<li><a href="setting.htm" style="padding: 5px 5px;"><span class="fa fa-cog">&nbsp;Setting<span></a></li>';
+                                  echo '<li><a href="setting.htm" style="padding: 5px 5px;"><span class="fa fa-cog">&nbsp;Setting</span></a></li>';
                           }
                         }
                       }
                      
                         if($interface == "help.php")
                         {
-                                echo '<li class="active"><a href="help.htm" style="padding: 5px 5px;"><span class="fa fa-rss "><span>&nbsp;Help</a></li>';
+                                echo '<li class="active"><a href="help.htm" style="padding: 5px 5px;"><span class="fa fa-rss "></span>&nbsp;Help</a></li>';
                         }
                         else
                         { 
-                                echo '<li><a href="help.htm" style="padding: 5px 5px;"><span class="fa fa-rss "><span>&nbsp;Help</a></li>';
+                                echo '<li><a href="help.htm" style="padding: 5px 5px;"><span class="fa fa-rss "></span>&nbsp;Help</a></li>';
                         }
                      
                     echo'
                         <li class="divider"></li>
 
-                        <li><a href="logout.htm" style="padding: 5px 5px;"><span class="fa fa-circle-o-notch"><span>&nbsp;Logout</a></li>
+                        <li><a href="logout.htm" style="padding: 5px 5px;"><span class="fa fa-circle-o-notch"></span>&nbsp;Logout</a></li>
                     </ul>
                 </li>
                 
