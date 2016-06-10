b0995c50aba1953f384afb87c41400fa9bbca24a
taseen <taseen@Innovative-Lenovo-PC>
2016-06-10 10:47:16 +0530

    look and feel all frame outer php

--- beta/src/php/manage_account_thirdparty.php
+++ beta/src/php/manage_account_thirdparty.php
@@ -2,7 +2,12 @@
 	include_once('util_session_variable.php');
 	include_once('util_php_mysql_connectivity.php'); 	
 	$js_function_name = "manage_show_file";    // FUNCTION NAME  
-	echo '<center>
+         echo'<ol class="breadcrumb">
+                <li><a href="#">Manage</a></li>
+                <li class="active"><b>Managing ThirdParty Accounts</b>                  
+                </li>
+            </ol>';
+	/*echo '<center>
 			<form name="manage1">
 				<fieldset class="manage_fieldset">
 					<legend><strong>Managing ThirdParty Accounts</strong></legend>
@@ -15,6 +20,20 @@
 							</tr>
 						</table>     
 				</fieldset>
+				<div style"display:none;" id="edit_div"> </div>
+			</form>
+		</center>'; */
+         echo '<center>
+			<form name="manage1">				
+						<table border="0" class="manage_interface" align="center">
+							<tr>
+								<td>
+									<input type="radio" name="new_exist" value="new" onclick="'.$js_function_name.'(\'src/php/manage_assign_account_thirdparty.php\')"/> Assign/DeAssign ThirdParty &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;
+									
+								</td>
+							</tr>
+						</table>     
+				
 				<div style"display:none;" id="edit_div"> </div>
 			</form>
 		</center>'; 
