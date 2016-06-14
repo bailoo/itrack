56410c3eb48f455ddf3a7147dfb0fc8b36275ca3
taseen <taseen@Innovative-Lenovo-PC>
2016-06-14 15:55:46 +0530

    look and feel all frame outer php

--- beta/src/php/manage_route.php
+++ beta/src/php/manage_route.php
@@ -2,7 +2,7 @@
 	include_once('util_session_variable.php');
 	include_once('util_php_mysql_connectivity.php'); 	
 	$js_function_name = "manage_show_file";    // FUNCTION NAME  
-	echo '<center>
+	/*echo '<center>
 			<form name="manage1">
 				<fieldset class="manage_fieldset">
 					<legend><strong>Route Assignment</strong></legend>
@@ -21,5 +21,23 @@
 			
     echo '<div style="height:5px;display:none;" id="automatic"></div>';  
 			
-	echo'</center>';  
+	echo'</center>';*/
+          echo'<form name="manage1">
+     <ol class="breadcrumb">
+                <li><a href="#">Manage</a></li>
+                <li class="active"><b>Route Assignment</b> 
+                    <div id="tab" class="btn-group" data-toggle="buttons" >
+                        <a onclick="'.$js_function_name.'(\'src/php/manage_assignment_route_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                           <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_assignment_route_prev.php\');document.getElementById(\'automatic\').style.display=none;document.getElementById(\'manual\').style.display=none;"/><i class="fa fa-link" aria-hidden="true"></i> Assign 
+                        </a>
+                       
+                    </div>
+                </li>
+            </ol>';
+ echo'	
+                <center>
+                    <div style"display:none;" id="edit_div"> </div>
+                </center>
+	</form>
+';
 ?>  
