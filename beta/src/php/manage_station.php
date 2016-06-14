90b0c2fce15a3705395fba9e9f36879919319b51
taseen <taseen@Innovative-Lenovo-PC>
2016-06-14 13:12:48 +0530

    look and feel all frame outer php

--- beta/src/php/manage_station.php
+++ beta/src/php/manage_station.php
@@ -2,7 +2,7 @@
 	include_once('util_session_variable.php');
 	include_once('util_php_mysql_connectivity.php'); 	
 	$js_function_name = "manage_show_file";    // FUNCTION NAME  
-	echo '<center>
+	/*echo '<center>
 			<form name="manage1">
 				<fieldset class="manage_fieldset">
 					<legend><strong>Station</strong></legend>
@@ -20,7 +20,39 @@
 				</fieldset>
 				<div style"display:none;" id="edit_div"> </div>
 			</form>';
+        */
+     echo'<form name="manage1">
+     <ol class="breadcrumb">
+                <li><a href="#">Manage</a></li>
+                <li class="active"><b>Station</b> 
+                    <div id="tab" class="btn-group" data-toggle="buttons" >
+                            <a onclick="'.$js_function_name.'(\'src/php/manage_add_station.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                               <input type="radio" name="new_exist" value="new" onclick="'.$js_function_name.'(\'src/php/manage_add_station.php\')"/><i class="fa fa-plus-square" aria-hidden="true"></i> Add
+                            </a>
+                            <a onclick="'.$js_function_name.'(\'src/php/manage_edit_station_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                              <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_edit_station_prev.php\');document.getElementById(\'automatic\').style.display=none;document.getElementById(\'manual\').style.display=none;"/><i class="fa fa-pencil" aria-hidden="true"></i> Edit
+                            </a>
+                            <a onclick="'.$js_function_name.'(\'src/php/manage_assignment_station_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                              <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_assignment_station_prev.php\');document.getElementById(\'automatic\').style.display=none;document.getElementById(\'manual\').style.display=none;"/><i class="fa fa-link" aria-hidden="true"></i> Assign
+                            </a>
+                            <a onclick="'.$js_function_name.'(\'src/php/manage_deassignment_station_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                              <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_deassignment_station_prev.php\');document.getElementById(\'automatic\').style.display=none;document.getElementById(\'manual\').style.display=none;"/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assign 
+                            </a>
+                            <a onclick="'.$js_function_name.'(\'src/php/manage_delete_station_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                              <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_delete_station_prev.php\');document.getElementById(\'automatic\').style.display=none;document.getElementById(\'manual\').style.display=none;"/><i class="fa fa-minus-square" aria-hidden="true"></i> Delete
+                            </a>
+                    </div>
+                </li>
+            </ol>';
+ echo'
+	
+            
 			
+                <center>
+                    <div style"display:none;" id="edit_div"> </div>
+                </center>
+	</form>
+';
 			
     echo '<div style="height:5px;display:none;" id="automatic">
 
