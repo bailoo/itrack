1243304ca70758c268c0b5dc55ccdf5ba6c3f6c4
taseen <taseen@Innovative-Lenovo-PC>
2016-06-14 16:08:25 +0530

    look and feel all frame outer php

--- beta/src/php/manage_secondary_vehicle.php
+++ beta/src/php/manage_secondary_vehicle.php
@@ -4,7 +4,7 @@
 	$js_function_name = "manage_show_file_1";    // FUNCTION NAME
 	$action_type1=$_POST['action_type']; 
 	//echo "Action Type=".$action_type1."<br>";
-		echo '<center>
+		/*echo '<center>
 	     <div style="height:10px"> </div> 			
 				<fieldset class="manage_fieldset">
 					<legend><strong>Secondary Vehicle</strong></legend>
@@ -36,5 +36,51 @@
 						</table>     
 				</fieldset>		
 		</center>';  
+               */ 
+        echo'
+            <ol class="breadcrumb">
+                <li><a href="#">Manage</a></li>
+                <li class="active"><b>Secondary Vehicle</b> 
+                <div id="tab" class="btn-group" data-toggle="buttons" >
+                    ';
+                    if($action_type1=="assign")
+                    {
+                            echo'
+                                <a onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_assign.php\',\'assign\')" class="btn btn-default active" data-toggle="tab" style="padding: 3px 12px;">
+                                    <input type="radio" name="new_exist" value="add" onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_assign.php\',\'assign\')" checked/><i class="fa fa-link" aria-hidden="true"></i> Assign
+                                </a>
+                                <a onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_deassign.php\',\'deassign\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                                    <input type="radio" name="new_exist" value="edit_delete" onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_deassign.php\',\'deassign\')"/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assign
+                                </a>
+                            ';
+                    }
+                    else if($action_type1=="deassign")
+                    {                	
+                            echo'
+                                <a onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_assign.php\',\'assign\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                                    <input type="radio" name="new_exist" value="add" onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_assign.php\',\'assign\')"/><i class="fa fa-link" aria-hidden="true"></i> Assign
+                                </a>
+                                <a onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_deassign.php\',\'deassign\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                                    <input type="radio" name="new_exist" value="edit_delete" onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_deassign.php\',\'deassign\')" checked/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assign
+                                </a>
+                            ';
+                    }
+                    else if($action_type1=="")
+                    {
+                            echo'
+                                <a onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_assign.php\',\'assign\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                                    <input type="radio" name="new_exist" value="add" onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_assign.php\',\'assign\')"/><i class="fa fa-link" aria-hidden="true"></i> Assign
+                                </a>
+                                <a onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_deassign.php\',\'deassign\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                                    <input type="radio" name="new_exist" value="edit_delete" onclick="'.$js_function_name.'(\'src/php/manage_secondary_vehicle_deassign.php\',\'deassign\')"/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assign
+                                </a>';
+                    }
+                                                                
+                    echo'
+                </div>
+                </li>
+            </ol>';
+           
+                                                  
 	?>
 	
