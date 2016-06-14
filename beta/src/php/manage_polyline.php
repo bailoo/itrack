d5f7d70690f82d54a97cc635fc4d7b1b635627db
taseen <taseen@Innovative-Lenovo-PC>
2016-06-14 11:45:15 +0530

    look and feel all frame outer php

--- beta/src/php/manage_polyline.php
+++ beta/src/php/manage_polyline.php
@@ -2,7 +2,8 @@
 	include_once('util_session_variable.php');
 	include_once('util_php_mysql_connectivity.php'); 	
 	$js_function_name = "manage_show_file";    // FUNCTION NAME  
-	echo '<center>
+	/*
+        echo '<center>
 			<form name="manage1">
 				<fieldset class="manage_fieldset">
 					<legend><strong>Ployline(Route)</strong></legend>
@@ -21,6 +22,37 @@
 				<div style"display:none;" id="edit_div"> </div>
 			</form>
 		</center>'; 
+        */
+        echo" <form name='manage1'>
+            <ol class='breadcrumb'>
+                <li><a href='#'>Manage</a></li>
+                <li class='active'><b>Ployline(Route)&nbsp;</b>
+                 <div id='tab' class='btn-group' data-toggle='buttons' >
+                 ";
+                echo'
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_add_polyline.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                    <input type="radio" name="new_exist" value="new" onclick="'.$js_function_name.'(\'src/php/manage_add_polyline.php\')"/><i class="fa fa-plus-square" aria-hidden="true"></i> Add Polyline
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_edit_polyline_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_edit_polyline_prev.php\')"/><i class="fa fa-pencil" aria-hidden="true"></i> Edit Polyline
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_assignment_polyline_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_assignment_polyline_prev.php\')"/><i class="fa fa-link" aria-hidden="true"></i>  Assign Vehicle
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_deassignment_polyline_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_deassignment_polyline_prev.php\')"/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assign Vehicle
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_register_polyline_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_register_polyline_prev.php\')"/><i class="fa fa-check-square-o" aria-hidden="true"></i> Register Polyline
+                   </a>
+                  </div>
+                 </li>
+            </ol>
+                <center>
+                    <div style"display:none;" id="edit_div"> </div>	
+                </center>
+            </form>
+            ';
 include_once('manage_loading_message.php');
  
 ?>  
--- beta/src/php/manage_polyline_history.php
+++ beta/src/php/manage_polyline_history.php
@@ -2,7 +2,7 @@
 	include_once('util_session_variable.php');
 	include_once('util_php_mysql_connectivity.php'); 	
 	$js_function_name = "manage_show_file";    // FUNCTION NAME  
-	echo '<center>
+	/*echo '<center>
 			<form name="manage1">
 				<fieldset class="manage_fieldset">
 					<legend><strong>Route(Polyline) From History</strong></legend>
@@ -21,7 +21,37 @@
 				</fieldset>
 				<div style"display:none;" id="edit_div"> </div>
 			</form>
-		</center>'; 
+		</center>'; */
+        echo" <form name='manage1'>
+            <ol class='breadcrumb'>
+                <li><a href='#'>Manage</a></li>
+                <li class='active'><b>Route(Polyline) From History&nbsp;</b>
+                 <div id='tab' class='btn-group' data-toggle='buttons' >
+                 ";
+                echo'
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_add_polyline_history.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                    <input type="radio" name="new_exist" value="new" onclick="'.$js_function_name.'(\'src/php/manage_add_polyline_history.php\')"/><i class="fa fa-plus-square" aria-hidden="true"></i> Add Polyline/Route
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_edit_polyline_prev_history.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_edit_polyline_prev_history.php\')"/><i class="fa fa-pencil" aria-hidden="true"></i> Edit Polyline/Route
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_assignment_polyline_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_assignment_polyline_prev.php\')"/><i class="fa fa-link" aria-hidden="true"></i>  Assign Vehicle
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_deassignment_polyline_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_deassignment_polyline_prev.php\')"/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assign Vehicle
+                   </a>
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_register_polyline_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_register_polyline_prev.php\')"/><i class="fa fa-check-square-o" aria-hidden="true"></i> Register Polyline
+                   </a>
+                  </div>
+                 </li>
+            </ol>
+                <center>
+                    <div style"display:none;" id="edit_div"> </div>	
+                </center>
+            </form>
+            ';
 include_once('manage_loading_message.php');
  
 ?>  
