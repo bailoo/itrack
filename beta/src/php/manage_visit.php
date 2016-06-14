2ec88016219cefbe5593f11ca40fc6ace74fbb25
taseen <taseen@Innovative-Lenovo-PC>
2016-06-14 13:54:18 +0530

    look and feel all frame outer php

--- beta/src/php/manage_visit.php
+++ beta/src/php/manage_visit.php
@@ -3,7 +3,7 @@
 	include_once('util_php_mysql_connectivity.php'); 
 
 	$js_function_name = "manage_show_file";    // FUNCTION NAME  
-	echo '<center>
+	/*echo '<center>
 			<form name="manage1">
 				<fieldset class="manage_fieldset">
 					<legend><strong>Schedule Visit</strong></legend>
@@ -22,5 +22,29 @@
 				</fieldset>
 				<div style"display:none;" id="edit_div"> </div>
 			</form>
-		</center>';   
+		</center>'; 
+        */
+        echo'<form name="manage1">
+     <ol class="breadcrumb">
+                <li><a href="#">Manage</a></li>
+                <li class="active"><b>Schedule Visit</b> 
+                    <div id="tab" class="btn-group" data-toggle="buttons" >
+                        <a onclick="'.$js_function_name.'(\'src/php/manage_add_visit_person_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                           <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_add_visit_person_prev.php\')"/><i class="fa fa-plus-square" aria-hidden="true"></i> Add Schedule
+                        </a>
+                        <a onclick="'.$js_function_name.'(\'src/php/manage_edit_visit_person_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                          <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_edit_visit_person_prev.php\')"/><i class="fa fa-pencil" aria-hidden="true"></i> Edit Schedule
+                        </a>
+                    </div>
+                </li>
+            </ol>';
+ echo'
+	
+            
+			
+                <center>
+                    <div style"display:none;" id="edit_div"> </div>
+                </center>
+	</form>
+';
 ?>  
