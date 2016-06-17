01d15f079b81e4d000f8422316b2980f76c79597
taseen <taseen@Innovative-Lenovo-PC>
2016-06-17 16:35:37 +0530

    look and feel all frame outer php

--- beta/src/php/manage_radio_group_account.php
+++ beta/src/php/manage_radio_group_account.php
@@ -8,14 +8,16 @@
 	$RowNo=0;
 	echo "<br>
 	<form name='manage1'>
-		<table border=1 cellspacing=0 cellpadding=0 class='module_tree_hierarchy' rules='all'>";
+		<!--<table border=1 cellspacing=0 cellpadding=0 class='module_tree_hierarchy' rules='all'>-->
+                <table cellspacing=0 cellpadding=0 class='table-bordered table-hover module_tree_hierarchy'>
+                ";
 			account_column_count($root);
-			echo"<tr>";
+			echo"<thead class='alert-warning'><tr>";
 					for($i=0;$i<$MaxColumnNo;$i++)
 					{
-						echo"<td>&nbsp;".'Level'.$i."</td>";
+						echo"<th>&nbsp;".'Level'.$i."</th>";
 					}
-			echo"</tr>";
+			echo"</tr></thead>";
 
 			$ColumnNo=0;
 			$RowNo=0;
