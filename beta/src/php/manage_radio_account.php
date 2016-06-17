8940ceb308ebf12d3904b59e2baa29abca7bd61f
taseen <taseen@Innovative-Lenovo-PC>
2016-06-17 16:08:20 +0530

    look and feel all frame outer php

--- beta/src/php/manage_radio_account.php
+++ beta/src/php/manage_radio_account.php
@@ -5,14 +5,16 @@
 			<tr>
 				<td>	
   <div style='height:370px;overflow:auto'>	
-			<table border=1 cellspacing=0 cellpadding=0 class='module_tree_hierarchy' align='center' rules='all'>";
+			<!--<table border=1 cellspacing=0 cellpadding=0 class='module_tree_hierarchy' align='center' rules='all'>-->
+                        <table cellspacing=0 cellpadding=0 class='table-bordered table-hover module_tree_hierarchy'>
+                        ";
 				account_column_count($root);
-				echo"<tr>";
+				echo"<thead class='alert-warning'><tr>";
 						for($i=0;$i<$MaxColumnNo;$i++)
 						{
-							echo"<td>&nbsp;".'Level'.$i."</td>";
+							echo"<th>&nbsp;".'Level'.$i."</th>";
 						}
-				echo"</tr>";
+				echo"</tr></thead>";
 
 				$ColumnNo=0;
 				$RowNo=0;
