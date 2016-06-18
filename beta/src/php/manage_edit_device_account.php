fce8218fe8456716e2678540b76ee4581133c2cf
taseen <taseen@Innovative-Lenovo-PC>
2016-06-18 12:57:32 +0530

    look and feel all frame outer php

--- beta/src/php/manage_edit_device_account.php
+++ beta/src/php/manage_edit_device_account.php
@@ -5,10 +5,12 @@
 	$root=$_SESSION['root'];  
 	echo "edit##"; 
 	include_once('tree_hierarchy_information.php');
+	echo'<table align="center" width="90%"><tr><td>';
 	include_once('manage_radio_account.php');
+        echo'</td></tr></table>';
 	echo'<br>
 	<center>		
-		<input type="button" value="Enter" onclick="javascript:manage_edit_prev(\'src/php/manage_edit_device.php\');">&nbsp;			
+		<input type="button" class="btn btn-default" value="Enter" onclick="javascript:manage_edit_prev(\'src/php/manage_edit_device.php\');">&nbsp;			
 	</center>';
 	include_once('manage_loading_message.php');
 	
