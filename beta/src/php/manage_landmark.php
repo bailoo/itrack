261c9117a60b36944cd9dc473b588ae2c7e2357f
taseen <taseen@Innovative-Lenovo-PC>
2016-06-20 17:38:46 +0530

    look and feel all frame outer php

--- beta/src/php/manage_landmark.php
+++ beta/src/php/manage_landmark.php
@@ -23,16 +23,16 @@
 			</form>
 		</center>'; */
          echo" <form name='manage1'>
-            <ol class='breadcrumb'>
-                <li><a href='#'>Manage</a></li>
-                <li class='active'><b>Landmark&nbsp;</b>
+            <ol class='breadcrumb' style='background-color:#f7fafc;padding:1px 15px;border-radius:0px'>
+                
+                <li class='active'><b>Landmark</b>&nbsp;&nbsp;&nbsp;
                  <div id='tab' class='btn-group' data-toggle='buttons' >
                  ";
                   echo'
-                   <a onclick="'.$js_function_name.'(\'src/php/manage_add_landmark.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_add_landmark.php\');" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
                     <input type="radio" name="new_exist" value="new" onclick="'.$js_function_name.'(\'src/php/manage_add_landmark.php\')"/><i class="fa fa-plus-square" aria-hidden="true"></i> Add
                    </a>
-                   <a onclick="'.$js_function_name.'(\'src/php/manage_edit_landmark_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_edit_landmark_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
 		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_edit_landmark_prev.php\')"/><i class="fa fa-pencil" aria-hidden="true"></i> Edit
                    </a>
                   </div>
