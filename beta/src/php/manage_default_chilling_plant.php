7e03b0a4628d236abda53a6fcfb9f71743fb5817
taseen <taseen@Innovative-Lenovo-PC>
2016-06-20 16:47:29 +0530

    look and feel all frame outer php

--- beta/src/php/manage_default_chilling_plant.php
+++ beta/src/php/manage_default_chilling_plant.php
@@ -23,14 +23,14 @@
 	</form>
 </center>'; */
 echo'<form name="manage1">
-     <ol class="breadcrumb">
-                <li><a href="#">Manage</a></li>
-                <li class="active"><b>Default Chilling Plant</b> 
+     <ol class="breadcrumb" style="background-color:#f7fafc;padding:1px 15px;border-radius:0px">
+                
+                <li class="active"><b>Default Chilling Plant</b>&nbsp;&nbsp; 
                     <div id="tab" class="btn-group" data-toggle="buttons" >
-                        <a onclick="'.$js_function_name.'(\'src/php/manage_default_chilling_plant_assignment_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                        <a onclick="'.$js_function_name.'(\'src/php/manage_default_chilling_plant_assignment_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
                            <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_default_chilling_plant_assignment_prev.php\')"/><i class="fa fa-link" aria-hidden="true"></i> Assignment
                         </a>
-                        <a onclick="'.$js_function_name.'(\'src/php/manage_default_chilling_plant_deassignment_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                        <a onclick="'.$js_function_name.'(\'src/php/manage_default_chilling_plant_deassignment_prev.php\')" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
                           <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_default_chilling_plant_deassignment_prev.php\')"/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assignment
                         </a>
                     </div>
