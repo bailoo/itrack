e2d95d0e3678ae4b91ca271e11628673efd2eeff
taseen <taseen@Innovative-Lenovo-PC>
2016-06-20 17:34:59 +0530

    look and feel all frame outer php

--- beta/src/php/manage_geofence.php
+++ beta/src/php/manage_geofence.php
@@ -21,22 +21,22 @@
 			</form>
 		</center>'; */
         echo" <form name='manage1'>
-            <ol class='breadcrumb'>
-                <li><a href='#'>Manage</a></li>
-                <li class='active'><b>Geofence&nbsp;</b>
+            <ol class='breadcrumb' style='background-color:#f7fafc;padding:1px 15px;border-radius:0px'>
+                
+                <li class='active'><b>Geofence</b>&nbsp;&nbsp;&nbsp;
                  <div id='tab' class='btn-group' data-toggle='buttons' >
                  ";
          echo'
-                   <a onclick="'.$js_function_name.'(\'src/php/manage_add_geofence.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_add_geofence.php\');" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
                     <input type="radio" name="new_exist" value="new" onclick="'.$js_function_name.'(\'src/php/manage_add_geofence.php\')"/><i class="fa fa-plus-square" aria-hidden="true"></i> Add
                    </a>
-                   <a onclick="'.$js_function_name.'(\'src/php/manage_edit_geofence_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_edit_geofence_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
 		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_edit_geofence_prev.php\')"/><i class="fa fa-pencil" aria-hidden="true"></i> Edit
                    </a>
-                   <a onclick="'.$js_function_name.'(\'src/php/manage_assignment_geofence_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_assignment_geofence_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
 		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_assignment_geofence_prev.php\')"/><i class="fa fa-link" aria-hidden="true"></i> Assign
                    </a>
-                   <a onclick="'.$js_function_name.'(\'src/php/manage_deassignment_geofence_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 3px 12px;">
+                   <a onclick="'.$js_function_name.'(\'src/php/manage_deassignment_geofence_prev.php\');" class="btn btn-default" data-toggle="tab" style="padding: 1px 12px;">
 		    <input type="radio" name="new_exist" value="exist" onclick="'.$js_function_name.'(\'src/php/manage_deassignment_geofence_prev.php\')"/><i class="fa fa-chain-broken" aria-hidden="true"></i> De-Assign
                    </a>
                   </div>
