ab775685195174b3defa5139de844bbf38583068
taseen <taseen@192.168.1.16>
2016-06-24 15:55:16 +0530

    look and feel all frame outer php

--- beta/src/php/report_radio_account.php
+++ beta/src/php/report_radio_account.php
@@ -8,14 +8,15 @@
 	$RowNo=0;
 	echo "<br>
 	<div style='height:300px;overflow:auto'>
-		<table border=1 cellspacing=0 cellpadding=0 class='module_left_menu' align='center' width='70%'>";
+		<!--<table border=1 cellspacing=0 cellpadding=0 class='module_left_menu' align='center' width='70%'>-->
+                <table cellspacing=0 cellpadding=0 class='table-bordered table-hover module_tree_hierarchy'>";
 			account_column_count($root);
-			echo"<tr>";
+			echo"<thead class='alert-warning'><tr>";
 					for($i=0;$i<$MaxColumnNo;$i++)
 					{
-						echo"<td>&nbsp;".'Level'.$i."</td>";
+						echo"<th>&nbsp;".'Level'.$i."</th>";
 					}
-			echo"</tr>";
+			echo"</tr></thead>";
 
 			$ColumnNo=0;
 			$RowNo=0;
