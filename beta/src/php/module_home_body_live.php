466abf6a7c8d46b46bc446fb4649493eb5897707
taseen <taseen@192.168.1.20>
2016-06-29 12:42:51 +0530

    look and feel all frame outer php

--- beta/src/php/module_home_body_live.php
+++ beta/src/php/module_home_body_live.php
@@ -9,7 +9,7 @@
 <input type="checkbox" id="trail_path_real"><span style="font-size:11px;color:green;">
 <b>Add Trail </b>
 </span>
-<font size=12px>Route(if any):</font><span id="selected_routes" style="display:none;"></span>
+<font size=1px >Route(if any):</font><span id="selected_routes" style="display:none;"></span>
 </div>
 <?php    
     echo ' <!--MAP DETAIL -->
