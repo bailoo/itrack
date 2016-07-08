096c73c28059e78f43633adb9edf30adac0454de
Admin <Admin@DESKTOP-JP07MFR>
2016-07-08 18:54:51 +0530

    action_report_history_report.php

--- beta/src/php/action_report_history_distance.php
+++ beta/src/php/action_report_history_distance.php
@@ -10,6 +10,19 @@
     include_once("report_title.php");
     include_once("calculate_distance.php");
 
+    function get_All_Dates($fromDate, $toDate, &$userdates) {
+        $dateMonthYearArr = array();
+        $fromDateTS = strtotime($fromDate);
+        $toDateTS = strtotime($toDate);
+
+        for ($currentDateTS = $fromDateTS; $currentDateTS <= $toDateTS; $currentDateTS += (60 * 60 * 24)) {
+            // use date() and $currentDateTS to format the dates in between
+            $currentDateStr = date("Y-m-d", $currentDateTS);
+            $dateMonthYearArr[] = $currentDateStr;
+            //print $currentDateStr.�<br />�;
+        }
+        $userdates = $dateMonthYearArr;    
+    }
 	//====cassamdra //////////////
     /*include_once('xmlParameters.php');
     include_once('parameterizeData.php'); /////// for seeing parameters
