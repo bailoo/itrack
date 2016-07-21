c582db78290f5d9ad1b5cec4ba51c52c31fc6177
Shams <Shams@Lenovo-PC>
2016-07-21 18:11:32 +0530

    data log option

--- beta/src/php/googleMapApi.php
+++ beta/src/php/googleMapApi.php
@@ -1027,7 +1027,7 @@ var input = (document.getElementById('pac-input'));
 									'<td class=\"live_td_css2\">'+tmp_address+'</td></tr>'+	
                                                                          '<tr>'+
                                                                         '<td>'+
-                                                                        '<a href=\"report.htm?dataLogImei='+vSerial+'&start_date_map='+start_date_map+'&end_date_map='+end_date_map+'\" target=\"_blank\">DataLog</a>'+
+                                                                        '<a href=\"report.htm?dataLogImei='+vSerial+'&start_date_map=abc&end_date_map=xyz'\" target=\"_blank\">DataLog</a>'+
                                                                         '</td>'+
                                                                         '</tr>'+
 									'</table>'+
