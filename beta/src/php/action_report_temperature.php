8e4286d719569ae90277d98854f23132da2c95e8
Shams <Shams@Lenovo-PC>
2016-07-22 18:26:22 +0530

    add graph in temperature report

--- beta/src/php/action_report_temperature.php
+++ beta/src/php/action_report_temperature.php
@@ -1,6 +1,6 @@
 ﻿<?php
-	//error_reporting(-1);
-	//ini_set('display_errors', 'On');
+	error_reporting(-1);
+	ini_set('display_errors', 'On');
 	set_time_limit(300);	
 	date_default_timezone_set("Asia/Kolkata");
 	include_once("main_vehicle_information_1.php");
