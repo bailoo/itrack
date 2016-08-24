a2347c91b1d043ac4cd2d5f3e1d324d3c942046a
Shams <Shams@192.168.1.15>
2016-08-24 10:22:04 +0530

    android download issues

--- beta/android/downloadApkUrl.php
+++ beta/android/downloadApkUrl.php
@@ -14,28 +14,39 @@ $apkHeading=$_GET['aH'];
 $downloadFileName=$_GET['dFN'];
 //$S3Filename =$_POST['sourceFilePath'];
 
+function getFileForDownload($apkType,$versionName,$apkHeading,$downloadFileName)
+{
+    $S3Filename="android/".$apkType."/".$versionName."/".$apkHeading."/".$downloadFileName;
+    //echo "serverFilePath=".$S3Filename."<br>";
+    $sourcefileNameArr=listFile($S3Filename);
+    //print_r($sourcefileNameArr);
+    $sourceFilePath=$S3Filename."/".$sourcefileNameArr[0]['name'];
 
-$S3Filename="android/".$apkType."/".$versionName."/".$apkHeading."/".$downloadFileName;
-//echo "serverFilePath=".$S3Filename."<br>";
-$sourcefileNameArr=listFile($S3Filename);
-//print_r($sourcefileNameArr);
-$sourceFilePath=$S3Filename."/".$sourcefileNameArr[0]['name'];
+    //echo "fileName=".$sourcefileNameArr[0]['name']."<br>";
+    $destinationFileName=$sourcefileNameArr[0]['name'];
+    $tmpFilePath="tmpFolder/".$destinationFileName;
 
-//echo "fileName=".$sourcefileNameArr[0]['name']."<br>";
-$destinationFileName=$sourcefileNameArr[0]['name'];
-$tmpFilePath="tmpFolder/".$destinationFileName;
+    $overwrite=true;
+    $copyResult=copyFile($sourceFilePath,$tmpFilePath,$overwrite);
+    $fileArr[]=array('filePath'=>$tmpFilePath,'fileName'=>$sourcefileNameArr[0]['name']);
+    return $fileArr;
+}
 
-$overwrite=true;
-$copyResult=copyFile($sourceFilePath,$tmpFilePath,$overwrite);
-if(count($sourcefileNameArr)>0)
+function downloadApkFile($filePath,$fileName)
 {
     header('Content-Type: application/jar');
     header('Content-Type: application/apk');
-    header('Content-Disposition: attachment; filename="'.$sourcefileNameArr[0]['name'].'"');
-    header('Content-Length: ' . filesize ($tmpFilePath));
-    readfile($tmpFilePath);
+    header('Content-Disposition: attachment; filename="'.$fileName.'"');
+    header('Content-Length: ' . filesize ($filePath));
+    readfile($filePath);
     //unlink($tmpFilePath); 
-    return true;
+    return true; 
+}
+
+$fileArr=getFileForDownload($apkType,$versionName,$apkHeading,$downloadFileName);
+if(count($fileArr)>0)
+{
+    downloadApkFile($fileArr[0]['filePath'],$fileArr[0]['fileName']);
 }
 else
 {
