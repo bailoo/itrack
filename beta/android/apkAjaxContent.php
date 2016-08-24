f8354a300b4bae37a5151fa14020a8cfefb34902
Shams <Shams@192.168.1.15>
2016-08-24 11:00:20 +0530

    android download issues

--- beta/android/apkAjaxContent.php
+++ beta/android/apkAjaxContent.php
@@ -66,7 +66,18 @@ while($statement->fetch())
     }
     $filePath="android/".$apk_type."/".$apk_version_name."/".$apk_heading."/".$download_file_name;
     
-    $downloadPathLink="http://www.itracksolution.com/android/downloadApkUrl.php?aT=".$apk_type."&vN=".$apk_version_name."&aH=".$apk_heading."&dFN=".$download_file_name;
+    $S3Filename="android/".$apk_type."/".$apk_version_name."/".$apk_heading."/".$download_file_name;
+    //echo "serverFilePath=".$S3Filename."<br>";
+    $sourcefileNameArr=listFile($S3Filename);
+    //print_r($sourcefileNameArr);
+    $sourceFilePath=$S3Filename."/".$sourcefileNameArr[0]['name'];
+    $destinationFileName=$sourcefileNameArr[0]['name'];
+    $tmpFilePath="tmpFolder/".$destinationFileName;
+    $overwrite=true;
+    $copyResult=copyFile($sourceFilePath,$tmpFilePath,$overwrite);
+    //$downloadPathLink="http://www.itracksolution.com/android/downloadApkUrl.php?aT=".$apk_type."&vN=".$apk_version_name."&aH=".$apk_heading."&dFN=".$download_file_name;
+    $downloadPathLink="http://www.itracksolution.com/android/downloadApkUrl.php?aT=".$destinationFileName;
+   
     echo '<tr>';
     echo '<td>'.$sNo.'</td>';
     echo '<td>'.$downloadPathLink.'</td>';
