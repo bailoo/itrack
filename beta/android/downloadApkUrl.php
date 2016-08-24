15d9a464bd90456ae8781fdab5bb5b84c53fae05
Shams <Shams@192.168.1.15>
2016-08-24 11:07:33 +0530

    android download issues

--- beta/android/downloadApkUrl.php
+++ beta/android/downloadApkUrl.php
@@ -2,7 +2,6 @@
 $fileName=$_GET['fileName'];
 $filePath="tmpFolder/".$fileName;
 
-header('Content-Type: application/jar');
 header('Content-Type: application/apk');
 header('Content-Disposition: attachment; filename="'.$fileName.'"');
 header('Content-Length: ' . filesize ($filePath));
