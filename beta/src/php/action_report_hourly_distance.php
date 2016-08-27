978d4fc4650967c89664fae172ce8ed151167b17
Shams <Shams@192.168.1.42>
2016-08-27 13:24:03 +0530

    add vehicle number column in map window

--- beta/src/php/action_report_hourly_distance.php
+++ beta/src/php/action_report_hourly_distance.php
@@ -3,7 +3,7 @@ include_once("main_vehicle_information_1.php");
  include_once('Hierarchy.php');
 include_once('util_session_variable.php');
 include_once('util_php_mysql_connectivity.php');
-
+date_default_timezone_set("Asia/Kolkata");
 
 echo'<link rel="StyleSheet" href="src/css/menu.css">';
 include_once('xmlParameters.php');
@@ -56,7 +56,14 @@ else if($personOption=="multiplePerson")
 $mysqlTableColumns="";
 $mysqlDistTableColumns="";
 $switchFlag=0;
-
+$sheetData = array(
+    array(
+      'a1 data',
+      'b1 data',
+      'c1 data',
+      'd1 data',
+    )
+  );
 for($i=$durationFrom;$i<=$durationTo;$i++) // for making dynamic column duration half hour fetching record from mysql
 {
 	$hr=($i<10)?'0'.$i:$i;
@@ -292,6 +299,35 @@ echo"</tr>";
 $serial++;
 	}		
 echo"</table></center>";
-exit();
+
+define('EOL', (PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+include_once ('PHPExcel/IOFactory.php');
+
+$cacheMethod = PHPExcel_CachedObjectStorageFactory::cache_to_phpTemp;
+$cacheSettings = array('memoryCacheSize' => '1028MB');
+PHPExcel_Settings::setCacheStorageMethod($cacheMethod, $cacheSettings);
+
+$sheet = array(
+    array(
+      'a1 data',
+      'b1 data',
+      'c1 data',
+      'd1 data',
+    )
+  );
+
+  $doc = new PHPExcel();
+  $doc->setActiveSheetIndex(0);
+
+  $doc->getActiveSheet()->fromArray($sheet, null, 'A1');
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment;filename="your_name.xls"');
+header('Cache-Control: max-age=0');
+
+  // Do your stuff here
+  $writer = PHPExcel_IOFactory::createWriter($doc, 'Excel5');
+
+$writer->save('php://output');
+//exit();
 		
 			
