0c3d0aaceaae2b56c6922ec050c39ce6b4e1531f
taseen <taseen@Innovative-Lenovo-PC>
2016-09-05 15:18:18 +0530

    rawmilk

--- beta/src/js/manage.js
+++ beta/src/js/manage.js
@@ -10477,7 +10477,7 @@ function close_vehicle_list(value)
 	document.getElementById(param1).style.display = "none";
 	document.getElementById(param2).style.display = "none";
 	
-    invoice_lorry_unique(serial,put_string.toUpperCase() );
+    //invoice_lorry_unique(serial,put_string.toUpperCase() ); //temporary blocked
 	
 	
 }
