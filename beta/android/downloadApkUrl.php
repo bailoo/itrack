01240bfe560e0b083059330376cccd62f344ffcf
Shams <Shams@Lenovo-PC>
2016-09-14 11:18:37 +0530

    download apk file

--- beta/android/downloadApkUrl.php
+++ beta/android/downloadApkUrl.php
@@ -1,11 +1,39 @@
 <?php
 $fileName=$_GET['fileName'];
-$filePath="tmpFolder/".$fileName;
+$tmpFilePath="tmpFolder/".$fileName;
 
-header('Content-Type: application/apk');
-header('Content-Disposition: attachment; filename="'.$fileName.'"');
-header('Content-Length: ' . filesize ($filePath));
-readfile($filePath);
-unlink($filePath); 
-return true;
+if($fd = fopen ($tmpFilePath, "r")) 
+{
+    $fsize = filesize($tmpFilePath);
+    $path_parts = pathinfo($tmpFilePath);
+
+    $path_parts["basename"]=$fileName;
+    $ext = strtolower($path_parts["extension"]);
+
+    switch ($ext) 
+    {
+        case "pdf":
+        header("Content-type: application/pdf"); // add here more headers for diff. extensions
+        header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a download
+        break;
+        default;
+        header("Content-type: application/octet-stream");
+        header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
+    }
+    header("Content-length: $fsize");
+    header("Cache-control: private"); //use this to open files directly
+
+    while(!feof($fd)) 
+    {
+        $buffer = fread($fd, 2048);
+        echo $buffer;
+    }
+    fclose ($fd);
+    unlink($tmpFilePath); 
+}
+else
+{
+        echo 'file not found';
+}
+
 ?>
