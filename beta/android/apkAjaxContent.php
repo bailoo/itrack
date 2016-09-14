1a1830d64a76f960b207446b05ea5871840a3993
Shams <Shams@Lenovo-PC>
2016-09-14 12:44:32 +0530

    download apk file

--- beta/android/apkAjaxContent.php
+++ beta/android/apkAjaxContent.php
@@ -76,94 +76,35 @@ while($statement->fetch())
                                     "filePath"=>$filePath  
                                 );
 }
-
-foreach ($apkDetailArr as $key => $values) {
-$keyValue=explode("###",$key);
-echo $keyValue[0]." ".$keyValue[1];
-    foreach ($values as $anotherkey => $val) {
-        echo 'downloadPathLink:'.$val['downloadPathLink']. ' download_file_name: '.$val['download_file_name'].' filePath:'.$val['filePath'].'<br>';
-    }
-
-}
-
-print_r($apkDetailArr);
-
 $statement->close();
 
 
 
 
-/*while($statement->fetch()) 
+foreach ($apkDetailArr as $key => $values) 
 {
+    
+    $keyValue=explode("###",$key);
     if($version_name!=$apk_version_name)
     {         
-        echo '<center><div><b>'.$apk_version_name.'</b></div></center><br>';    
+        echo '<center><div><b>'.$keyValue[0].'</b></div></center><br>';    
     }
-    if($apk_heading!=$cmpHeading)
-    {
-        //echo "in if1<br>";
-        $sNo=1;
-        echo'<div style="margin-left :19%;"><b>&nbsp;'.$apk_heading.'</b></div><br>';
-        echo'<center><table border="1" class="menu" cellspacing="3" cellpadding="3" rules="all" bordercolor="grey">';  
-    }
-    $filePath="android/".$apk_type."/".$apk_version_name."/".$apk_heading."/".$download_file_name;
-    
-    $S3Filename="android/".$apk_type."/".$apk_version_name."/".$apk_heading."/".$download_file_name;
-    //echo "serverFilePath=".$S3Filename."<br>";
-    $sourcefileNameArr=listFile($S3Filename);
-    //print_r($sourcefileNameArr);
-    $sourceFilePath=$S3Filename."/".$sourcefileNameArr[0]['name'];
-    $destinationFileName=$sourcefileNameArr[0]['name'];
-    $tmpFilePath="tmpFolder/".$destinationFileName;
-    $overwrite=true;
-    $copyResult=copyFile($sourceFilePath,$tmpFilePath,$overwrite);
-    //$downloadPathLink="http://www.itracksolution.com/android/downloadApkUrl.php?aT=".$apk_type."&vN=".$apk_version_name."&aH=".$apk_heading."&dFN=".$download_file_name;
-    $downloadPathLink="http://www.itracksolution.com/android/downloadApkUrl.php?fileName=".$destinationFileName;
-   
-    echo '<tr>';
-    echo '<td>'.$sNo.'</td>';
-    echo '<td>'.$downloadPathLink.'</td>';
-     echo '<td>'.$download_file_name.'</td>';
-    echo "<td> &nbsp;
-            <a href='#' onclick='javascript:downloadApkFile1(\"".$filePath."\",\"".$download_file_name."\");'>
-                Download
-            </a>
-        </td>";
-    echo '</tr>';
-    if($apk_heading!=$cmpHeading && $sNo!=1)
-    {
-        //echo "in if2<br>";
-      echo'</table>';
-    }
-    $sNo++;
-    $version_name=$apk_version_name;
-    $cmpHeading=$apk_heading;
-    
-    }  
-echo "</table></center>";
-    $statement->close();
-
-
-/*for($i=0;$i<sizeof($listDir);$i++)
-{
-    $listDir1=listDir("android/".$apkType."/".$listDir[$i]);
-    echo "<br>";
-    //print_r($listDir1);
-    for($j=0;$j<sizeof($listDir1);$j++)
-    {
-        $listDir2=listDir("android/".$apkType."/".$listDir[$i]."/".$listDir1[$j]);
-        print_r($listDir2);
-        echo "<br>";
-        for($k=0;$k<sizeof($listDir2);$k++)
-        {
-            echo "android/".$apkType."/".$listDir[$i]."/".$listDir1[$j]."/".$listDir2[$k];
-            //$listFile=listFile("android/".$apkType."/".$listDir[$i]."/".$listDir[$j]."/".$listDir2[$k]);
-            echo "<br>";
-            //print_r($listFile);
-
-            //$finalArr=array('apdType'=>$listDir[$i],'heading'=>$listDir1[$j],'fileFolder'=>$listDir2[$k],'fileName'=>)
-        }
-    }
-}*/
-//echo "filePath=".$filePathToS3Wrapper."<br>";   */
+    echo'<div style="margin-left :19%;"><b>&nbsp;'.$keyValue[1].'</b></div><br>';
+    foreach ($values as $anotherkey => $val) 
+    {     
+    echo'<center>
+        <table border="1" class="menu" cellspacing="3" cellpadding="3" rules="all" bordercolor="grey">
+        <tr>
+        <td>'.$sNo.'</td>
+        <td>'.$val['downloadPathLink'].'</td>
+        <td>'.$val['download_file_name'].'</td>';
+        echo "<td> &nbsp;
+                <a href='#' onclick='javascript:downloadApkFile1(\"".$val['filePath']."\",\"".$val['download_file_name']."\");'>
+                    Download
+                </a>
+            </td>";
+        echo '</tr>
+        </table>';
+     }
+}  
 ?>
