0bd2a12767024da3b41d35af506e3437ae8b20da
Rizwan <Rizwan@LAPTOP-GJC2A499>
2016-11-03 16:25:41 +0530

    coreDb

--- beta/src/php/coreDb.php
+++ beta/src/php/coreDb.php
@@ -5448,7 +5448,7 @@ function getGCM_Id_Detail($imei, $apk_version, $DbConnection)
 {
     $gcm_id = "";
     $query="SELECT gcm_id FROM gcm_data WHERE imei='$imei' AND version='$apk_version' AND status=1";
-    echo "query=".$query."<br>";
+    //echo "query=".$query."<br>";
     $result=mysql_query($query,$DbConnection);
     //$row_result=mysql_num_rows($result);
     if($row=mysql_fetch_object($result))
