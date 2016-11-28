a2eb2af7729ff6d18f7153656a6236c8bad4fae4
sparwez <shams.parwez@iembsys.com>
2016-11-28 14:17:19 +0530

    download apk

--- beta/android/downloadApkUrl.php
+++ beta/android/downloadApkUrl.php
@@ -2,38 +2,11 @@
 $fileName=$_GET['fileName'];
 $tmpFilePath="tmpFolder/".$fileName;
 
-if($fd = fopen ($tmpFilePath, "r")) 
-{
-    $fsize = filesize($tmpFilePath);
-    $path_parts = pathinfo($tmpFilePath);
-
-    $path_parts["basename"]=$fileName;
-    $ext = strtolower($path_parts["extension"]);
-
-    switch ($ext) 
-    {
-        case "pdf":
-        header("Content-type: application/pdf"); // add here more headers for diff. extensions
-        header("Content-Disposition: attachment; filename=\"".$path_parts["basename"]."\""); // use 'attachment' to force a download
-        break;
-        default;
-        header("Content-type: application/octet-stream");
-        header("Content-Disposition: filename=\"".$path_parts["basename"]."\"");
-    }
-    header("Content-length: $fsize");
-    header("Cache-control: private"); //use this to open files directly
-
-    while(!feof($fd)) 
-    {
-        $buffer = fread($fd, 2048);
-        echo $buffer;
-    }
-    fclose ($fd);
-    //unlink($tmpFilePath); 
-}
-else
-{
-        echo 'file not found';
-}
+header('Content-Type: application/vnd.android.package-archive');
+header("Content-length: " . filesize($tmpFilePath));
+header('Content-Disposition: attachment; filename="' . $fileName . '"');
+ob_end_flush();
+readfile($tmpFilePath);
+return true;
 
 ?>
