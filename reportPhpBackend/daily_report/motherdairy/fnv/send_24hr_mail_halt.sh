f7a25cf0a1c04f6488a8241c95cd52a249c9132c
rizcompeer <rizwan@iembsys.com>
2021-12-09 11:12:34 +0530

    Delete reportPhpBackend directory

--- reportPhpBackend/daily_report/motherdairy/fnv/send_24hr_mail_halt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /mnt/itrack/reportPhpBackend/daily_report/motherdairy/fnv
-php mail_halt_24hr_report.php
-
-
--- reportPhpBackend/hourly_report/delhi/copy_master.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo scp -i /mnt/upload/iTrack.pem -r /mnt/itrack/beta/src/php/gps_report/231/master ubuntu@54.175.45.115:/mnt/itrack/beta/src/php/gps_report/231/
-
-
