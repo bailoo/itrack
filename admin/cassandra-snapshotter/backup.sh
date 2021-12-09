df6142183800271504d70cc470fc01cc808fc688
rizcompeer <rizwan@iembsys.com>
2021-12-09 11:13:53 +0530

    Delete admin/cassandra-snapshotter directory

--- admin/cassandra-snapshotter/backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cassandra-snapshotter --aws-access-key-id=AKIAJAQNRFIKAJH4RRFQ --aws-secret-access-key=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 --s3-bucket-name=itrack --s3-bucket-region=ap-southeast-1 --s3-base-path=itrackcass backup --hosts=52.74.33.255 --user=ubuntu
